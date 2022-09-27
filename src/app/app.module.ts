381f06e1545e0e39c8136ed383fc815f06d77264
AleksandreGzirishvili <sangzirishvili@hotmail.com>
2022-09-27 22:00:15 +0400

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SinginComponent } from './singin/singin.component';
+import { CommunityComponent } from './community/community.component';
+import { SupportComponent } from './support/support.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SinginComponent,
+    CommunityComponent,
+    SupportComponent
   ],
   imports: [
     BrowserModule,
