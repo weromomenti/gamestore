ce751102a1561431487b38b7964ea6780cfe96ee
Giorgi Tsereteli <Giorgi_Tsereteli@epam.com>
2022-09-29 21:33:37 +0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -8,6 +7,8 @@ import { AboutComponent } from './about/about.component';
 import { SinginComponent } from './singin/singin.component';
 import { CommunityComponent } from './community/community.component';
 import { SupportComponent } from './support/support.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GameComponent } from './games/components/games.component';
 
 @NgModule({
   declarations: [
@@ -16,13 +17,19 @@ import { SupportComponent } from './support/support.component';
     AboutComponent,
     SinginComponent,
     CommunityComponent,
-    SupportComponent
+    SupportComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    NavbarComponent,
+    GameComponent
+  ]
 })
 export class AppModule { }
